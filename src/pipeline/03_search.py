a1df34198b3c90da69b0820dd0c04d3fa0d98afa
bearsgom-commits <bearsgom@gmail.com>
2026-02-11 22:52:03 +0900

    Update 03_search.py

--- src/pipeline/03_search.py
+++ src/pipeline/03_search.py
@@ -1,1 +1,6 @@
+import numpy as np
 
+def semantic_search(query_emb, corpus_emb, topk=10):
+    scores = np.dot(query_emb, corpus_emb.T)
+    indices = np.argsort(-scores, axis=1)[:, :topk]
+    return indices
